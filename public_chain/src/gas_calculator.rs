fd6fcd897811e8e22e72b82ba50bba9f2aec0e1f
yennhan96 <leowyennhan@gmail.com>
2023-12-07 18:14:11 +0800

    Update gas calculators

--- public_chain/src/gas_calculator.rs
+++ public_chain/src/gas_calculator.rs
@@ -7,6 +7,9 @@ const GAS_COST_CONTRACT_CREATION: u64 = 32000;
 const GAS_COST_CONTRACT_INTERACTION: u64 = 45000;
 const GAS_COST_TOKEN_TRANSFER: u64 = 50000;
 // Add other gas cost constants as needed.
+const GAS_PER_BYTE: u64 = 10; // Example cost per byte
+const GAS_PER_FUNCTION_CALL: u64 = 100; // Base cost for function call
+
 
 /// Calculate gas for a simple ETH transfer.
 pub fn calculate_gas_for_simple_transfer() -> u64 {
@@ -14,44 +17,32 @@ pub fn calculate_gas_for_simple_transfer() -> u64 {
 }
 
 /// Calculate gas for contract creation.
-pub fn calculate_gas_for_contract_creation() -> u64 {
-    GAS_COST_CONTRACT_CREATION
+pub fn calculate_gas_for_contract_creation(data: &[u8],function_call: u64) -> u64 {
+    let data_size_gas = data.len() as u64; // Example, replace with actual gas calculation
+    let function_call_gas = function_call; // Example, replace with actual gas calculation
+
+    let mut gas_used = 0;
+    gas_used += data_size_gas;
+    gas_used += function_call_gas;
+
+    // Now return the calculated gas
+    gas_used
 }
 
 /// Calculate gas for interacting with a contract.
-pub fn calculate_gas_for_contract_interaction() -> u64 {
-    GAS_COST_CONTRACT_INTERACTION
+pub fn calculate_gas_for_contract_interaction(data: &[u8],function_cost:u64) -> u64 {
+    let data_size_gas = data.len() as u64; // Example, replace with actual gas calculation
+    let function_call_gas = function_cost; // Example, replace with actual gas calculation
+
+    let mut gas_used = 0;
+    gas_used += data_size_gas;
+    gas_used += function_call_gas;
+
+    // Now return the calculated gas
+    gas_used
 }
 
 /// Calculate gas for a token transfer.
 pub fn calculate_gas_for_token_transfer() -> u64 {
     GAS_COST_TOKEN_TRANSFER
-}
-
-/// Calculate total gas for a series of operations.
-///
-/// # Arguments
-///
-/// * `operations` - A vector of operations for which to calculate total gas.
-pub fn calculate_total_gas(operations: Vec<&str>) -> u64 {
-    operations.iter().map(|&op| match op {
-        "simple_transfer" => calculate_gas_for_simple_transfer(),
-        "contract_creation" => calculate_gas_for_contract_creation(),
-        "contract_interaction" => calculate_gas_for_contract_interaction(),
-        "token_transfer" => calculate_gas_for_token_transfer(),
-        // Handle other operations...
-        _ => 0,
-    }).sum()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_total_gas_calculation() {
-        let operations = vec!["simple_transfer", "contract_creation", "token_transfer"];
-        let total_gas = calculate_total_gas(operations);
-        assert_eq!(total_gas, GAS_COST_SIMPLE_TRANSFER + GAS_COST_CONTRACT_CREATION + GAS_COST_TOKEN_TRANSFER);
-    }
 }
