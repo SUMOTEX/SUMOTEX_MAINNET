056b088059e745c20b133d6a87095fca644e5045
yennhan96 <leowyennhan@gmail.com>
2023-12-08 14:50:18 +0800

    Added gas cost

--- public_chain/src/gas_calculator.rs
+++ public_chain/src/gas_calculator.rs
@@ -1,6 +1,6 @@
 // gas_calculator.rs
 use wasmparser::{Parser, Payload, Operator};
-use std::collections::HashSet;
+use std::collections::{HashSet,HashMap};
 use std::fs::File;
 use std::io::Read; 
 use std::fs;
@@ -14,6 +14,53 @@ const GAS_COST_TOKEN_TRANSFER: u64 = 50000;
 const GAS_PER_BYTE: u64 = 10; // Example cost per byte
 const GAS_PER_FUNCTION_CALL: u64 = 100; // Base cost for function call
 
+
+fn opcode_gas_costs() -> HashMap<String, u64> {
+    let mut map = HashMap::new();
+    map.insert("I32Const".to_string(), 2);  // Example cost for I32Const
+    map.insert("I32Store8".to_string(), 4); // Example cost for I32Store8
+    map.insert("I32Load8U".to_string(), 3); // Example cost for I32Load8U
+    map.insert("Call".to_string(), 10);     // Example cost for Call
+    map.insert("BrIf".to_string(), 5);      // Example cost for BrIf
+    map.insert("I32Store16".to_string(), 4); // Example cost for I32Store16
+    map.insert("I32Load".to_string(), 3);    // Example cost for I32Load
+    map.insert("I32Store".to_string(), 4);   // Example cost for I32Store
+    map.insert("I32Load16U".to_string(), 3); // Example cost for I32Load16U
+    map.insert("I64Const".to_string(), 2);   // Example cost for I64Const
+    map.insert("I64Load8U".to_string(), 3);  // Example cost for I64Load8U
+    map.insert("I64Store".to_string(), 4);   // Example cost for I64Store
+    map.insert("LocalSet".to_string(), 2);    // Example cost for LocalSet
+    map.insert("LocalGet".to_string(), 2);    // Example cost for LocalGet
+    map.insert("Br".to_string(), 5);          // Example cost for Br
+    map.insert("BrTable".to_string(), 6);     // Example cost for BrTable
+    map.insert("I32Add".to_string(), 3);      // Example cost for I32Add
+    map.insert("I32Sub".to_string(), 3);      // Example cost for I32Sub
+    map.insert("I32Mul".to_string(), 3);      // Example cost for I32Mul
+    map.insert("I32DivS".to_string(), 5);     // Example cost for I32DivS
+    map.insert("I32DivU".to_string(), 5);     // Example cost for I32DivU
+    map.insert("I32RemS".to_string(), 5);     // Example cost for I32RemS
+    map.insert("I32RemU".to_string(), 5);     // Example cost for I32RemU
+    map.insert("I32And".to_string(), 3);      // Example cost for I32And
+    map.insert("I32Or".to_string(), 3);       // Example cost for I32Or
+    map.insert("I32Xor".to_string(), 3);      // Example cost for I32Xor
+    map.insert("I32Shl".to_string(), 3);      // Example cost for I32Shl
+    map.insert("I32ShrS".to_string(), 3);     // Example cost for I32ShrS
+    map.insert("I32ShrU".to_string(), 3);     // Example cost for I32ShrU
+    map.insert("I32Eqz".to_string(), 2);      // Example cost for I32Eqz
+    map.insert("I32Eq".to_string(), 2);       // Example cost for I32Eq
+    map.insert("I32Ne".to_string(), 2);       // Example cost for I32Ne
+    map.insert("I32LtS".to_string(), 2);      // Example cost for I32LtS
+    map.insert("I32LtU".to_string(), 2);      // Example cost for I32LtU
+    map.insert("I32GtS".to_string(), 2);      // Example cost for I32GtS
+    map.insert("I32GtU".to_string(), 2);      // Example cost for I32GtU
+    map.insert("I32LeS".to_string(), 2);      // Example cost for I32LeS
+    map.insert("I32LeU".to_string(), 2);      // Example cost for I32LeU
+    map.insert("I32GeS".to_string(), 2);      // Example cost for I32GeS
+    map.insert("I32GeU".to_string(), 2);      // Example cost for I32GeU
+
+    // ... Add other opcodes and their gas costs
+    map
+}
 
 struct GasCalculator {
     base_cost: u64,
@@ -61,9 +108,32 @@ pub fn disassemble_wasm(file_path: &str) ->Result<(), Box<dyn std::error::Error>
         println!("{}", opcode);
     }
 
+    calculate_gas_by_opcode(&opcodes);
 
     Ok(())
 }
+
+fn calculate_gas_by_opcode(opcodes: &HashSet<String>) {
+    let mut total_gas = 0;
+    let gas_map = opcode_gas_costs();
+
+    for opcode in opcodes {
+        // Extract the base opcode name. For example, "Call { function_index: 55 }" becomes "Call".
+        let base_opcode = opcode.split_whitespace().next().unwrap_or("");
+
+        if let Some(&gas) = gas_map.get(base_opcode) {
+            total_gas += gas;
+            println!("Opcode: {:?}, Gas cost: {}", base_opcode, gas);
+        } else {
+            println!("No gas cost found for base opcode {}", base_opcode);
+        }
+    }
+
+    println!("Total gas for all opcodes: {}", total_gas);
+}
+
+
+
 /// Calculate gas for contract creation.
 pub fn calculate_gas_for_contract_creation(data: &[u8],function_call: u64) -> u64 {
     let data_size_gas = data.len() as u64; // Example, replace with actual gas calculation
@@ -88,9 +158,4 @@ pub fn calculate_gas_for_contract_interaction(data: &[u8],function_cost:u64) -> 
 
     // Now return the calculated gas
     gas_used
-}
-
-/// Calculate gas for a token transfer.
-pub fn calculate_gas_for_token_transfer() -> u64 {
-    GAS_COST_TOKEN_TRANSFER
 }
