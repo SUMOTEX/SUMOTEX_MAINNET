e6efa42fc8d3ad578a4c8d7eb4fb9c8708cf00b5
yennhan96 <leowyennhan@gmail.com>
2024-04-05 23:04:22 +0800

    ok

--- public_chain/src/main.rs
+++ public_chain/src/main.rs
@@ -228,7 +228,7 @@ async fn main() {
         }
         loop {
             // if let Some(port) = whitelisted_peers.pop() {
-                let address_str = format!("/ip4/{}/tcp/8100",(my_local_ip.to_string()));
+                let address_str = format!("/ip4/{}/tcp/8101",(my_local_ip.to_string()));
                 let the_address = Multiaddr::from_str(&address_str).expect("Failed to parse multiaddr");  
                 println!("{}",the_address);      
                 //Loop  to listen
@@ -319,9 +319,10 @@ async fn main() {
                                         .expect("at least one peer")
                                         .to_string(),
                                 };
-                                let json = serde_json::to_string(&req).expect("can jsonify request");
+                                
                          
                             }
+                            let json = serde_json::to_string(&req).expect("can jsonify request");
                             swarm_public_net
                             .behaviour_mut()
                             .floodsub
